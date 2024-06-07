f02c1583c844c94763bfa051f32db3b8cc9d59b8
Jose Reyna <dariuzcm@gmail.com>
2024-06-07 17:50:26 -0600

    updating for get image source user error

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,4 +1,3 @@
-import { getPhoto } from "@/api/userServiceHandler";
 import { Avatar, AvatarImage } from "@/shadcdn/ui/avatar";
 import { FunctionComponent } from "react";
 
@@ -16,7 +15,7 @@ const User: FunctionComponent<UserProps> = (props) => {
   const { classNames } = props
   return <div className="flex flex-col justify-center items-center px-5 pb-3 gap-1">
     <Avatar className="size-14">
-      <AvatarImage src={getPhoto(props.src) || "/avatarEmpty.svg"} />
+      <AvatarImage src={props.src || "/avatarEmpty.svg"} />
     </Avatar>
     <h1 className={`font-semibold ${classNames.name}`}>{props.name}</h1>
     <h2 className="text-white-300">{props.description}</h2>
